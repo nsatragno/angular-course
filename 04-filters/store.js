052ff97b08e51abcc474f43511991bef98a356de
elfrasco <adrianparedes82@gmail.com>
2015-06-09 15:08:22 -0300

    Tabs added to fourth example

--- 04-filters/store.js
+++ 04-filters/store.js
@@ -24,4 +24,14 @@ angular.module('filtersApp', [])
     
         store.longPhrase = "Are you feeling lucky punk dyin' ain't much of a livin', boy. well, do you have anything to say for yourself? this is my gun, clyde! don't p!ss down my back and tell me it's raining. are you feeling lucky punk dyin' ain't much of a livin', boy. man's gotta know his limitations. don't p!ss down my back and tell me it's raining. here. put that in your report! and i may have found a way out of here. ever notice how sometimes you come across somebody you shouldn't have f**ked with? well, i'm that guy. this is my gun, clyde!";
     
+        var selectedTab = 'movies';
+    
+        store.isTabSelected = function(tab) {
+            return tab === selectedTab;
+        };
+    
+        store.selectTab = function(tab) {
+            selectedTab = tab;
+        };
+    
     });
